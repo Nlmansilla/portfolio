d9034bf193e31212913cee69a7f701d44b48f22c
Nicolas Mansilla <nlmansilla89@gmail.com>
2024-07-27 14:18:15 +0100

    Added page for error handling

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,8 +1,12 @@
 <template>
     <article class="prose max-w-none dark:prose-invert prose-pre:bg-white
      dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300">
-        <ContentDoc v-slot="{ doc }">
-            <div class="grid grid-cols-6 gap-16">
+        <ContentDoc>
+            <template #not-found>
+                <h1>Document not found</h1>
+            </template>
+            <template v-slot="{ doc }">
+                <div class="grid grid-cols-6 gap-16">
                 <div :class="{'col-span-4': doc.toc, 'col-span-6': !doc.toc}">
                     <ContentRenderer :value="doc" />
                 </div>
@@ -19,6 +23,7 @@
                     </aside>
                 </div>
             </div>
+            </template>
         </ContentDoc>
     </article>
 </template>
